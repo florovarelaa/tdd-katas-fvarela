1084aa9a632aa4998cb6d0595623a5fc505d96e7
floro <florovarelaa@gmail.com>
2024-10-12 12:51:41 -0300

    feat(PrimeFactors): kata-4

--- typescript/src/_copy_me_fvarela/PrimeFactors.test.ts
+++ typescript/src/_copy_me_fvarela/PrimeFactors.test.ts
@@ -31,4 +31,14 @@ describe(PrimeFactors, () => {
     // assert
     expect(result).toEqual([3]);
   });
+  it("`4` should return `[2,2]`", () => {
+    // arrange
+    const sut = new PrimeFactors();
+
+    // act
+    const result = sut.generate(4);
+
+    // assert
+    expect(result).toEqual([2,2]);
+  });
 });
--- typescript/src/_copy_me_fvarela/PrimeFactors.ts
+++ typescript/src/_copy_me_fvarela/PrimeFactors.ts
@@ -2,7 +2,13 @@ export class PrimeFactors {
   constructor() {}
 
   generate(number: number): number[] {
-    if (number === 1) return [];
-    return [number]
+    const result = [];
+    for (let divisor = 2; number > 1; divisor++) {
+      while (number % divisor === 0) {
+        result.push(divisor);
+        number = number / divisor;
+      }
+    }
+    return result;
   }
 }
